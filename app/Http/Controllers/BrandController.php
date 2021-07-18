f8a427e587f2a0c46c5fee4eee1cd4dbe67393a7
tatsuki okuda <t-okuda@lotus-gr.co.jp>
2021-07-18 16:20:42 +0900

    controllers middleware auth add

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -10,6 +10,12 @@ use Image;
 
 class BrandController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * brand index
      *
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,7 +11,10 @@ use Illuminate\Support\Facades\Redirect;
 
 class CategoryController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     public function AllCat()
     {
